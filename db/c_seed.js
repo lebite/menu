3546668fcaa4632501e0f0e44ec404a7977547f6
NJC <NJC@users.noreply.github.com>
2019-07-28 18:31:15 -0700

    optimize cassandra seed code

--- db/c_seed.js
+++ db/c_seed.js
@@ -3,60 +3,48 @@
 const faker = require('faker');
 const fs = require('fs');
 
-const writeCassandraFile = fs.createWriteStream('cassandra.csv');
+const writeCassandraFile = fs.createWriteStream('cassandra5.csv');
 
-const allMenus = ['Early', 'Dinner', 'Breakfast', 'Dinner', 'Seasonal', 'Lunch', 'Brunch', 'Lunch', 'Weekend Brunch', 'Happy Hour', 'Late Night', 'Beverages', 'Kids', 'Bar', 'Dessert', 'Cocktails', 'Gluten-Free', 'Vegetarian', 'Drinks'];
-const menuSections = ['Tromp', 'Bode', 'Zboncak', 'Dare', 'Olson', 'Crist', 'Bauch', 'Bode', 'Torp', 'Gulgowski', 'Leuschke', 
+
+function writeIntoFile(writer, encoding, callback) {
+  const allMenus = ['Early', 'Dinner', 'Breakfast', 'Dinner', 'Seasonal', 'Lunch', 'Brunch', 'Lunch', 'Weekend Brunch', 'Happy Hour', 'Late Night', 'Beverages', 'Kids', 'Bar', 'Dessert', 'Cocktails', 'Gluten-Free', 'Vegetarian', 'Drinks'];
+  const menuSections = ['Tromp', 'Bode', 'Zboncak', 'Dare', 'Olson', 'Crist', 'Bauch', 'Bode', 'Torp', 'Gulgowski', 'Leuschke', 
   'Bergnaum', 'Moen', 'Simonis', 'Kihn', 'Jast', 'Bahringer', 'Oberbrunner', 'Rolfson', 'Kunde', 'Walker', 'Maggio', 'Schowalter', 'Grimes',
   'Dare', 'Will', 'Kessler', 'Kemmer', 'Denesik', 'Jast'];
-
-
-const makeCassandraData = () => {
-  const result = [];
-  for (let i = 1; i < 10000000; i++) {
-    const menus = [allMenus[faker.random.number({ min: 0, max: 3 })], allMenus[faker.random.number({ min: 4, max: 7 })], allMenus[faker.random.number({ min: 7, max: allMenus.length - 1 })]];
-    for (const menu of menus) {
-      const sections = [menuSections[faker.random.number({ min: 0, max: 10 })], menuSections[faker.random.number({ min: 11, max: 20 })],
-        menuSections[faker.random.number({ min: 21, max: 29 })]]; 
-      for (let j = 0; j < faker.random.number({ min: 2, max: 3 }); j++) {
-        const section = sections[j];
-        for (let k = 0; k < faker.random.number({ min: 4, max: 5 }); k++) {
-          const item = `${i}, ${menu}, ${section}, ${faker.commerce.productName()}, ${faker.lorem.words()}, ${faker.commerce.price(5, 50, 2)}`;
-          result.push(item);
-        }
-      }
-    }
-  }
-  return result;
-};
-
-const cassandraData = makeCassandraData();
-
-function writeIntoFile(writer, data, encoding, callback) {
-  let i;
+  let i = 10000001; 
+  write();
   function write() {
     let ok = true;
     do {
-      for (i = data.length - 1; i >= 0; i--) {
-        if (i === 0) {
-          writer.write(`${data[i]}\n`, encoding, callback);
-        } else {
-          if (i % 100000 === 0) {
-            console.log(i);
-          }
-          ok = writer.write(`${data[i]}\n`, encoding);
-        }
+      i--;
+      if (i % 100000 === 0) {
+        console.log(i);
       }
-    } while (i > 0 && ok);
-    if (i > 0) {
+        const menus = [allMenus[faker.random.number({ min: 0, max: 3 })], allMenus[faker.random.number({ min: 4, max: 7 })], allMenus[faker.random.number({ min: 7, max: allMenus.length - 1 })]];
+        for (const menu of menus) {
+          const sections = [menuSections[faker.random.number({ min: 0, max: 10 })], menuSections[faker.random.number({ min: 11, max: 20 })],
+            menuSections[faker.random.number({ min: 21, max: 29 })]]; 
+          for (let j = 0; j < faker.random.number({ min: 2, max: 3 }); j++) {
+            const section = sections[j];
+            for (let k = 0; k < faker.random.number({ min: 4, max: 5 }); k++) {
+              const item = `${i}, ${menu}, ${section}, ${faker.commerce.productName()}, ${faker.lorem.words()}, ${faker.commerce.price(5, 50, 2)}`;
+              if (i === 1) {
+                writer.write(`${item}\n`, encoding, callback);
+              } else {
+                ok = writer.write(`${item}\n`, encoding);
+              }
+            }
+          }
+        }
+      } while (i  > 8000000 && ok);
+    if (i > 8000000) {
       writer.once('drain', write);
     }
   }
-  write();
 }
 
 
-// writeIntoFile(writeCassandraFile, cassandraData, 'utf8', () => console.log('write complete'));
+writeIntoFile(writeCassandraFile, 'utf8', () => {});
 
 
 
